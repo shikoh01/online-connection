eaf317d2dc9412aa460bee4449996d59997a5691
shikoh01 <116588106+shikoh01@users.noreply.github.com>
2022-10-24 21:52:39 +0300

    Add files via upload

---
+++ Javascript/script.js
@@ -0,0 +1,14 @@
+*//
+//alert("Hello Customer");
+//alert("Sorry for waiting");
+//console.log(prompt("Do You have any Question ?"));
+//var a= "We are future"
+
+//document.write(a);
+//function convert (){
+    //var Yes = document.getElementById('data').value;
+//alert("haider")
+//alert(5*10);
+
+let n=6
+console=.log(n == 6);
